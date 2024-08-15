a0b6ee688ba93e4b08a3aebda57a38d478e15e9c
Garvita Jhanwar <104294059+Garvita1602@users.noreply.github.com>
2024-08-15 23:11:43 +0530

    Update homePage.jsx

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,30 +1,34 @@
+import { useContext } from "react";
 import SearchBar from "../../components/searchBar/SearchBar";
+import { AuthContext } from "../../context/AuthContext";
 import "./homePage.scss";
 
 function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
-          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
+          <h1 className="title">Discover Your Perfect Home with HomeVista</h1>
+          <p className="xyz">
+            Welcome to HomeVista, your ultimate destination for finding the perfect home.
+            At HomeVista, we understand that a home is more than just a place to live; it is where life happens.
+            Discover your dream home with HomeVista, where finding the perfect home is just a click away.
           </p>
           <SearchBar />
           <div className="boxes">
             <div className="box">
-              <h1>16+</h1>
+              <h1>12+</h1>
               <h2>Years of Experience</h2>
             </div>
             <div className="box">
-              <h1>200</h1>
+              <h1>5</h1>
               <h2>Award Gained</h2>
             </div>
             <div className="box">
-              <h1>2000+</h1>
+              <h1>20000+</h1>
               <h2>Property Ready</h2>
             </div>
           </div>
